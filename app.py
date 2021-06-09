ebfdacf4432649426dfab610216ee70325e17c04
Martin <x00135214@gmail.com>
2021-06-09 13:40:40 +0100

    register.html addded with reg form

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -29,6 +30,10 @@ def get_all_quotes():
     quotes = mongo.db.quotes.find()
     return render_template("quotes.html", quotes=quotes)
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
