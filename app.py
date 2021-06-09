692dbfd604a0676b119345020097a2d928066f9a
Martin <x00135214@gmail.com>
2021-06-09 19:20:27 +0100

    wtf form added to registration

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-  
 import os
 from flask import (
     Flask, flash, render_template,
@@ -6,9 +5,10 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
 if os.path.exists("env.py"):
     import env
-
 
 app = Flask(__name__)
 
@@ -18,6 +18,11 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+class LoginForm(FlaskForm):
+    user_name = StringField('user_name')
+    user_password = PasswordField('user_password')
+    check_password = PasswordField('check_password')
+    user_email = StringField('user_email')
 
 @app.route("/")
 @app.route("/get_index")
@@ -33,7 +38,31 @@ def get_all_quotes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        return "<h1> The username is {}. The password is {}. The check is {}. The email is {}.".format(form.user_name.data, form.user_password.data, form.check_password.data, form.user_email.data)
+    
+    """
+    if request.method == "POST":
+    
+        # check if name/email already registered
+        existing_user = mongo.db.users.find_one(
+            {"user_name": request.form.get("user_name").lower()})
+        existing_email = mongo.db.users.find_one(
+            {"user_password": request.form.get("user_password").lower()})
+        
+        if existing_user or existing_email:
+            flash("Username or password already exist")
+            return redirect("register")
+        
+        register = {
+            "user_name": request.form.get("user_name").lower(),
+            "user_email": request.form.get("user_email").lower(),
+            "user_password": generate_password_hash(request.form.get("user_password"))
+        }
+    """
+    return render_template("register.html", form=form)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
