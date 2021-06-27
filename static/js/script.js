7bdc71fa06824036c4e04c4da31a5b05fe5336c4
Martin <x00135214@gmail.com>
2021-06-27 14:49:25 +0100

    back to top btn added

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,22 @@ $("#year").text(function () {
   let year = y.getFullYear();
   return year;
 });
+
+/* back to top btn */
+/* https://www.w3schools.com/howto/howto_js_scroll_to_top.asp */
+var mybutton = document.getElementById("top-btn");
+
+window.onscroll = function() {scrollFunction();};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
